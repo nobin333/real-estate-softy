dbd36c138686e85cd106f0bceabe6c5cacdc3092
nobin333 <sharifnobin333@gmail.com>
2025-09-25 03:31:43 +0600

    connect to mongodb

--- api/index.js
+++ api/index.js
@@ -1,5 +1,18 @@
 const express = require("express")
 const cors = require("cors")
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+
+dotenv.config()
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 
 const app = express()
