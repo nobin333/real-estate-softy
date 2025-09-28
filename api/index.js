4620c8cefa78d525e221ed1eaf21032d5f7aaa3e
nobin333 <sharifnobin333@gmail.com>
2025-09-29 02:25:41 +0600

    signup api route is created

--- api/index.js
+++ api/index.js
@@ -3,6 +3,12 @@ const cors = require("cors")
 const dotenv = require("dotenv")
 const mongoose = require("mongoose")
 
+const authRouter = require("../api/routes/auth.route")
+
+// import userRouter from './routes/user.route.js';
+// import authRouter from './routes/auth.route.js';
+// import listingRouter from './routes/listing.route.js';
+
 dotenv.config()
 
 mongoose
@@ -16,6 +22,14 @@ mongoose
 
 
 const app = express()
+
+app.use(express.json());
+
+// app.use(cookie-parser());
+
+// app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+// app.use('/api/listing', listingRouter);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
