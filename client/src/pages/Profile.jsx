69120ab5cb27efc63aa4baa59b5d5f411a1fcb96
nobin333 <sharifnobin333@gmail.com>
2026-01-18 12:36:57 +0600

    complete create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const Profile = () => {
     const { currentUser } = useSelector((state) => state.user);
 
-    
-//   const handleSignOut = async () => {
-//     try {
-//       dispatch(signOutUserStart());
-//       const res = await fetch('/api/auth/signout');
-//       const data = await res.json();
-//       if (data.success === false) {
-//         dispatch(deleteUserFailure(data.message));
-//         return;
-//       }
-//       dispatch(deleteUserSuccess(data));
-//     } catch (error) {
-//       dispatch(deleteUserFailure(data.message));
-//     }
-//   };
+
+    //   const handleSignOut = async () => {
+    //     try {
+    //       dispatch(signOutUserStart());
+    //       const res = await fetch('/api/auth/signout');
+    //       const data = await res.json();
+    //       if (data.success === false) {
+    //         dispatch(deleteUserFailure(data.message));
+    //         return;
+    //       }
+    //       dispatch(deleteUserSuccess(data));
+    //     } catch (error) {
+    //       dispatch(deleteUserFailure(data.message));
+    //     }
+    //   };
     return (
         <div className='p-3 max-w-lg mx-auto'>
             <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -53,6 +54,12 @@ const Profile = () => {
                 >
                     Update
                 </button>
+                <Link
+                    className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'
+                    to={'/create-listing'}
+                >
+                    Create Listing
+                </Link>
 
             </form>
             <div className='flex justify-between mt-5'>
@@ -61,7 +68,7 @@ const Profile = () => {
                 >
                     Delete account
                 </span>
-                <span  className='text-red-700 cursor-pointer'>
+                <span className='text-red-700 cursor-pointer'>
                     Sign out
                 </span>
             </div>
